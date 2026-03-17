8aba25b52c5f08dcff97ac35a2a3beb5da048ec4
Sascha Andres <sascha.andres@outlook.com>
2026-03-17 19:09:26 +0100

    feat: add receiver

---
+++ internal/jrnl/mail.go
@@ -0,0 +1,43 @@
+package jrnl
+
+import "iter"
+
+type (
+	Receiver struct {
+		server   string
+		port     int
+		user     string
+		password string
+
+		allowedSender             []string
+		deleteFromUnallowedSender bool
+	}
+
+	Mail struct {
+		Subject string
+		Body    string
+	}
+)
+
+// NewReceiver creates a new receiver.
+func NewReceiver(server string, port int, user string, password string) Receiver {
+	return Receiver{
+		server:   server,
+		port:     port,
+		user:     user,
+		password: password,
+	}
+}
+
+// Start starts the receiver.
+// It connects to the IMAP server and authenticates the user.
+func (r *Receiver) Start() error {}
+
+// Stop stops the receiver.
+// It disconnects from the IMAP server.
+func (r *Receiver) Stop() error {}
+
+// GetMails returns a sequence of mails in the inbox.
+func (r *Receiver) GetMails() iter.Seq[Mail] {
+	return nil
+}
